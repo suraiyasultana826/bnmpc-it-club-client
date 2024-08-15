54ad808867dc7a1af5a695cd8c743c4f9848b90f
suraiyasultana826 <suraiyasultana826@gmail.com>
2024-08-15 13:15:37 +0600

    added typewriter effect

--- src/Pages/Home/Banner/Bannerr.jsx
+++ src/Pages/Home/Banner/Bannerr.jsx
@@ -1,18 +1,25 @@
 import banner from '../../../assets/finalBanner.jpg'
+import Typewriter from 'typewriter-effect';
 const Bannerr = () => {
-    return (
-        <div className="hero  md:min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-          <img src={banner} className="w-full md:w-1/2 rounded-lg shadow-2xl" />
-          <div>
-            <h1 className="md:text-5xl text-3xl font-bold md:px-10">Welcome to BNMPC IT CLUB!</h1>
-            <p className="py-6 md:px-10 md:font-bold">Welcome to BNMPCITC, where collaboration meets innovation! Embrace the dynamic world of technology at our IT club, a hub for passionate minds and aspiring tech enthusiasts. Here, we foster creativity, collaboration, and cutting-edge exploration. Whether you are a coding maestro, a design virtuoso, or simply curious about the digital realm, BNMPCITC is your platform to thrive. 
-</p>
-           
-          </div>
+  return (
+    <div className="hero  md:min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={banner} className="w-full md:w-1/2 rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="md:text-5xl text-3xl font-bold md:px-10"><Typewriter
+            options={{
+              strings: ['Welcome To BNMPC IT Club'],
+              autoStart: true,
+              loop: true,
+            }}
+          /></h1>
+          <p className="py-6 md:px-10 md:font-bold">Welcome to BNMPCITC, where collaboration meets innovation! Embrace the dynamic world of technology at our IT club, a hub for passionate minds and aspiring tech enthusiasts. Here, we foster creativity, collaboration, and cutting-edge exploration. Whether you are a coding maestro, a design virtuoso, or simply curious about the digital realm, BNMPCITC is your platform to thrive.
+          </p>
+
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Bannerr;
